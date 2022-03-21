3b6bf9e88cf7b9012bf1f4107fa89ecc7f720dd7
Teun van Gils <edu@teunvg.nl>
2022-03-21 13:01:31 +0100

    Added comments and homework prompt to guessing game.

--- guessing_game.py
+++ guessing_game.py
@@ -1,13 +1,20 @@
-from random import randint
+from random import randint  # Import the randint function from the random package
 # import random
 
-number = randint(1, 10)
-print(number)
+number = randint(1, 10)  # Generate a random number between 1 and 10
+print(number)  # Print the number (to make testing easier)
 
+# Ask the user for a guess, and convert the result to an integer
 guess = int(input('Guess the number (between 1 and 10): '))
 
+# While the correct number was not guessed...
 while number != guess:
-    print('Incorrect...')
-    guess = int(input('Try again: '))
+    print('Incorrect...')  # Let the user know that their answer was incorrect
+    guess = int(input('Try again: '))  # And ask them to guess again
 
+# When the above while loop terminates, that means the correct number was guessed!
 print('Correct!')
+
+# Homework: extend the above program so that:
+#   1) it generates a number between 1 and 100,
+#   2) it gives higher/lower feedback on an incorrect guess.
